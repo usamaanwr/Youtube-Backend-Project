eefb0f9f30b95c01fe4f5b358250d9f57b6ca30b
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-03-10 01:49:57 +0500

    Implement database connection setup and update package scripts

---
+++ src/dp/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const connectDB = async () => {
+    try {
+        const conectionInstance = await mongoose.connect(`${process.env.MONGOBD_URI}`)
+        console.log(`\n MongoDB connected  !! DB HOST: ${conectionInstance.connection.connection}`);
+        
+    } catch (error) {
+        console.log("MONGO BD CONNECTION IS FAILED", error);
+        process.exit(1)
+    }
+}
+export default connectDB;
