28e0eb81f6dc665902566472908949889657a531
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-03-17 14:22:54 +0500

    Add video model with schema and aggregate pagination support

---
+++ src/models/vedio.model.js
@@ -0,0 +1,41 @@
+import mongoose , { Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const vedioSchema = new Schema({
+
+    vedioFile: {
+        type: String, // cludinary url,
+        required : true,
+    },
+    thumbnail:{
+        type: String, // cludinary url,
+        required : true,
+    },
+    title:{
+        type: String, 
+        required : true,
+    },
+    description:{
+        type: String, 
+        required : true,
+    },
+    duaration:{
+        type: Number,
+        required : true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+
+},{ timestamps: true})
+vedioSchema.plugin(mongooseAggregatePaginate)
+export const Vedio = mongoose.model("Vedio", vedioSchema) 
