652c03002516689b36f6d5716b731e83029369cb
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-03-17 14:22:39 +0500

    Fix server port configuration in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./dp/index.js";
-
+import {app} from "../src/app.js";
 connectDB()
-
+const port = process.env.PORT || 8000
 .then(()=>{
-    app.listen(process.env,PORT || 8000 , ()=>{
+    app.listen(process.env,port || 8000 , ()=>{
         console.log(`server is running at port : ${process.env.PORT}`);
         
     })
