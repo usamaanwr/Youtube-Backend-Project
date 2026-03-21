a7b64ae50cecc559a080db6b4b487b7f10426a95
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-03-22 01:35:12 +0500

    Fix MongoDB connection log to correctly reference the host property

--- src/dp/index.js
+++ src/dp/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 const connectDB = async () => {
     try {
         const conectionInstance = await mongoose.connect(`${process.env.MONGOBD_URI}`)
-        console.log(`\n MongoDB connected  !! DB HOST: ${conectionInstance.connection.connection}`);
+        console.log(`\n MongoDB connected  !! DB HOST: ${conectionInstance.connection.host}`);
         
     } catch (error) {
         console.log("MONGO BD CONNECTION IS FAILED", error);
