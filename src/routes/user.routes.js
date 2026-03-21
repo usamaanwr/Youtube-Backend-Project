e730fb053858b9c50307fa74df7272accb4a94aa
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-03-22 01:35:27 +0500

    Fix import path and enhance user registration route to include file uploads

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
-import { reqisterUser } from "../controllers/user.contollers.js";
+import { reqisterUser } from "../controllers/user.contoller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
-router.post('/register', reqisterUser)
+router.post('/register', upload.fields([{
+    name: "avatar",
+    maxCount: 1
+},{
+    name: "coverImage",
+    maxCount: 1
+}
+]),
+reqisterUser)
 
 export default router
